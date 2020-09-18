ef97d8ea5c37aaea8821a57cf4770e2dc47951d5
Linus Norton <linusnorton@gmail.com>
2020-09-18 13:49:12 +0100

    finished

--- src/GridFactory.d.ts
+++
@@ -1,4 +0,0 @@
-import { Grid } from "./Grid";
-export declare class GridFactory {
-    create(row: string): Grid;
-}
--- src/Solver.d.ts
+++
@@ -1,6 +0,0 @@
-import { Grid } from "./Grid";
-export declare class Solver {
-    solve(grid: Grid): string;
-    private eliminate;
-    private infer;
-}
